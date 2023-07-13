1263e67c4a281ac1199a83bee5b85b2807296434
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-13 15:32:11 -0500

    added controllers logic for login, register, and protected apis's

---
+++ src/controllers/authController.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ username }, 'secret_key');
+    res.json({ accessToken: token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error: ' + error });
+  }
+};
+
+exports.register = async (req, res) => {
+  const { username, password, email, role } = req.body;
+
+  try {
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: 'Email already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username,
+      password: hashedPassword,
+      email,
+      role: role | 'admin'
+    });
+
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error: ' + error });
+  }
+};
