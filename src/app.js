38f078994edb4493134d866fb776998513f2e681
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-13 15:33:57 -0500

    added main app.js file that defines middlewares to use, and creates node server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-require("./config/db");
-
-const express = require("express");
-const bodyparser = express.jason;
-const cors = require("cors");
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const protectedRoutes = require('./routes/protectedRoutes');
+const authenticateToken = require('./middlewares/authenticateToken');
+require('./config/database');
+require("dotenv").config();
 
 const app = express();
-app.use(cors());
-app.use(bodyparser());
+app.use(express.json());
 
-module.exports = app;
+// app routes
+app.use('/auth', authRoutes);
+app.use('/protected', authenticateToken, protectedRoutes);
+
+//start server at port PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server started on port 3000');
+});
